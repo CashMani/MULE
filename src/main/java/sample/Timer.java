fe8415a553cf9b9b78ecf7cbe4c47dbd2fc0f3e6
melliott9 <melliott9@gatech.edu>
2015-11-04 00:50:34 -0500

    Timer Class Javadoc

--- src/main/java/sample/Timer.java
+++ src/main/java/sample/Timer.java
@@ -7,6 +7,8 @@ import javafx.util.Duration;
 /**
  * Created by Alexandra Link on 9/30/15.
  * Barely edited by Mani on 9/30/15
+ *
+ * Times each Player's turn within the MULE game.
  */
 public class Timer {
 
@@ -14,26 +16,44 @@ public class Timer {
     Duration startTime;
     Duration stopTime;
 
+    /**
+     * Timer constructor
+     */
     public Timer() {
 
     }
 
+    /**
+     * Sets the duration of time a user will have to take a turn
+     * @param turnTime the allocated duration of time for a turn
+     */
     public void setTimeline(Duration turnTime) {
         startTime = turnTime;
         timeline = new Timeline(new KeyFrame(turnTime, ae -> GameController.endTurn()));
 
     }
 
+    /**
+     * Begins the timer.
+     */
     public void startTimer() {
         timeline.play();
     }
 
+    /**
+     * Stops the timer.
+     * @return the duration of time that has passed since the timer was started
+     */
     public Duration stopTimer() {
         stopTime = timeline.getCurrentTime();
         timeline.stop();
         return startTime.subtract(stopTime);
     }
 
+    /**
+     * Gets the time remaining based on the start time and allotted stop time
+     * @return the amount of time remaining for a turn
+     */
     public Duration getTimeRemaining() {
         return startTime.subtract(stopTime);
     }
